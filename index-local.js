eb6289508c9aea7eaae849c575ed0522114367f8
LUCAS LATANCIO POLIDORO <lucas.help@outlook.com>
2025-08-10 18:44:47 -0300

    Update index-local.js

--- index-local.js
+++ index-local.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function calcularNivel(vitorias, derrotas) {
   let saldoVitorias = vitorias - derrotas;
   let nivel = "";
@@ -24,29 +23,3 @@ function calcularNivel(vitorias, derrotas) {
 
 // Exemplo de chamada
 calcularNivel(90, 0); // Saída: O Herói tem de saldo de 90 está no nível de Diamante
-=======
-let nome = "Lucas"
-let quantidade = 4000
-let nivel  
-
-if (quantidade <= 1000) {
-    nivel = "Ferro"
-} else if (quantidade <= 2000) {
-    nivel = "Bronze"
-} else if (quantidade <= 5000) {
-    nivel = "Prata"
-} else if (quantidade <= 7000) {
-    nivel = "Ouro"
-} else if ( quantidade <= 8000) {
-    nivel = "Platina"
-} else if ( quantidade <= 9000) {
-    nivel = "Ascendente"
-} else if ( quantidade <= 10000) {
-    nivel = "Imortal"
-} else if ( quantidade > 10000) {
-    nivel = "Radiante"
-} else {
-
-}
-console.log("o herói de nome " + nome + " está no nível " + nivel)
->>>>>>> dc611b45ff355b5cfd89cb3eac37d54b31bb2dc3
